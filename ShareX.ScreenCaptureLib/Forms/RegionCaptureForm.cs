ff65c23c5396856dd2bc29fe6b898d39625c89a7
WhiteChairFromIkea <dainius.mazuika@gmail.com>
2022-02-17 22:12:26 +0200

    Usual question upon image editor closure: Do you want to save changes?

--- ShareX.ScreenCaptureLib/Forms/RegionCaptureForm.cs
+++ ShareX.ScreenCaptureLib/Forms/RegionCaptureForm.cs
@@ -598,6 +598,7 @@ namespace ShareX.ScreenCaptureLib
             }
         }
 
+        /* Return true if user confirms editor closure */
         internal bool ShowExitConfirmation()
         {
             bool result = true;
@@ -605,8 +606,21 @@ namespace ShareX.ScreenCaptureLib
             if (IsImageModified)
             {
                 Pause();
-                result = MessageBox.Show(this, Resources.RegionCaptureForm_ShowExitConfirmation_Text, Resources.RegionCaptureForm_ShowExitConfirmation_ShareXImageEditor,
-                    MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes;
+                DialogResult confirmationResult = MessageBox.Show(this, Resources.RegionCaptureForm_ShowExitConfirmation_Text, Resources.RegionCaptureForm_ShowExitConfirmation_SaveChanges,
+                    MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                switch (confirmationResult)
+                {
+                    case DialogResult.Yes:
+                        this.OnSaveImageRequested();
+                        break;
+                    case DialogResult.No:
+                        break; // Exit editor without saving
+                    case DialogResult.Cancel:
+                        result = false; // Return to editor;
+                        break;
+                }
+
                 Resume();
             }
 
